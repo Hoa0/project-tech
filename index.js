240c27ace2607e86fade7919e4cd760b278d041d
hoa0 <t-hoa.l@hotmail.com>
2021-02-11 12:23:48 +0100

    add 404

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('static'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!d')
 });
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
