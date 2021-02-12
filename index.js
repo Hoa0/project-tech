cbb4def2f97a0862b797b2d6f5f071cd60271e24
hoa0 <t-hoa.l@hotmail.com>
2021-02-12 13:00:47 +0100

    install nodemon and template engine pug

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!d')
 });
 
+app.get('/login', (req, res) => {
+  res.send('registered')
+});
+
+app.get('/login/:chefID', (req, res) => {
+  res.send(`<h1>Detailpage of chef ${req.params.chefId} </h1>`)
+});
+
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
 })
