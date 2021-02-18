f9e282ed6b7c4db9682b7e5f0ce23f11da605655
hoa0 <t-hoa.l@hotmail.com>
2021-02-18 12:49:02 +0100

    update

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ app.use('/public',express.static(path.join(__dirname,'static')));
 app.set('view engine','ejs');
 
 //route
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', function (req, res) {
+  res.render('index')
+});
+app.get('/search', (req, res) => {
+  res.render('search')
 });
 
-
-app.get('/:userQuery',(req,res)=>{
+app.get('/sushi/:userQuery',(req,res)=>{
     res.render('home',{data : {userQuery: req.params.userQuery,
-                               searchResults : ['book1','book2','book3'],
+                               searchResults : ['Sashimi','Temaki','California roll'],
                                loggedIn : true,
-                               username : 'yooo'}});
+                               username : 'Jan'}});
 });
 
+// page not found
 app.use(function (req, res, next) {
   res.status(404).send("Sorry can't find that!")
-})
+});
 
 // start server
 app.listen(port, () => {
