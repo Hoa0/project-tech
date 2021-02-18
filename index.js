bda5ae90ecd8a407398fe1cc677a0f39cb3488b8
hoa0 <t-hoa.l@hotmail.com>
2021-02-18 11:08:05 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
-
 //init app
 const app = express();
 const port = 3000;
 
-//home route
-app.get('/', function(req, res){
-  res.render('index');
-});
-
-// Load View Engine
-// ejs
-// https://www.youtube.com/watch?v=VM-2xSaDxJc
-// https://github.com/noobcoder1137/ejs_templates_express 
 app.use('/public',express.static(path.join(__dirname,'static')));
 app.set('view engine','ejs');
+
+//route
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+
 app.get('/:userQuery',(req,res)=>{
     res.render('home',{data : {userQuery: req.params.userQuery,
                                searchResults : ['book1','book2','book3'],
                                loggedIn : true,
                                username : 'yooo'}});
 });
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
 
 // start server
 app.listen(port, () => {
