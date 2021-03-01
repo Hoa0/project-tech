fb27e5a7254e16d6378267f9e4acab5f3247f3bb
hoa0 <t-hoa.l@hotmail.com>
2021-03-01 09:36:09 +0100

    eslint

--- README.md
+++ README.md
@@ -1,21 +1,34 @@
 # Project-tech
+
 Dit is de Repository voor project-tech in blok tech.
+
 ## Table of Content
-* Installaties
-* License
+
+- Installaties
+- License
+
 ## Installaties
+
 Wanneer u gebruik wil maken van dit project, moet u Node.js en npm packages installeren. Volg hieronder de volgende stappen.
+
 #### Om dit project op te halen naar uw computer, clone dit repo:
+
 ```commandline
 git clone https://github.com/Hoa0/project-tech.git
 ```
-#### NPM packages installeren 
+
+#### NPM packages installeren
+
 ```commandline
 npm install
 ```
+
 #### Om de server te laten runnen, voer de volgende commandline uit
+
 ```commandline
 npm start
 ```
+
 ## License
+
 Voor dit repository heb ik gebruik gemaakt van een [MIT License](https://github.com/Hoa0/project-tech/blob/main/LICENSE)
--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const path = require('path');
-const ejs = require('ejs');
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
 //init app
 const app = express();
 const port = 3000;
 
-app.use(express.static('static'));
-app.set('view engine','ejs');
+app.use(express.static("static"));
+app.set("view engine", "ejs");
 
 //route
-app.get('/', function (req, res) {
-  res.render('index.ejs')
+app.get("/", function (req, res) {
+  res.render("index.ejs");
 });
 
-app.get('/search', (req, res) => {
-  res.render('search')
+app.get("/search", (req, res) => {
+  res.render("search");
 });
 
-app.get('/test/:userQuery',(req,res)=>{
-    res.render('test',{data : {userQuery: req.params.userQuery,
-                               searchResults : ['Sashimi','Temaki','California roll'],
-                               loggedIn : true,
-                               username : 'Jan'}});
+app.get("/test/:userQuery", (req, res) => {
+  res.render("test", {
+    data: {
+      userQuery: req.params.userQuery,
+      searchResults: ["Sashimi", "Temaki", "California roll"],
+      loggedIn: true,
+      username: "Jan",
+    },
+  });
 });
 
 // page not found
 app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!")
+  res.status(404).send("Sorry can't find that!");
 });
 
 // start server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
